5702d481dc9efe391aa5d462fc05ee6f57b8070d
lklimkiewicz <lukaszklimkiewicz@Mac-mini-ukasz.local>
2016-12-07 22:35:16 +0100

    touch id

--- touchIdAutenthication/ViewController.swift
+++ touchIdAutenthication/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var authClicked: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+         //touchIdCall()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func authClickedAction(_ sender: Any) {
+        touchIdCall()
+    }
 
-
+    func touchIdCall(){
+        let authContext : LAContext = LAContext()
+        var error : NSError? = nil
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Testowanie touch id", reply: { (success, error) in
+                    if success {
+                        print(self.statusLabel.isHidden)
+                        self.statusLabel.isHidden = false
+                        self.statusLabel.text = "Odcisk prawidłowy"
+                    }else{
+                        self.statusLabel.isHidden = false
+                        self.statusLabel.text = "Odcisk nieprawidłowy"
+                    }
+            })
+        }else{
+            
+        }
+    }
+    
 }
 
